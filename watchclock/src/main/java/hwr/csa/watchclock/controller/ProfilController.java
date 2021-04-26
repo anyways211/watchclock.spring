d9c13630aafc9673b0f0ec09204488bdd4874ad7
annika <annika.pass@gmx.de>
2021-04-26 17:50:27 +0200

    links in profilUebersicht.html angepasst und Controller erstellt

---
+++ watchclock/src/main/java/hwr/csa/watchclock/controller/ProfilController.java
@@ -0,0 +1,16 @@
+package hwr.csa.watchclock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ProfilController {
+
+    @GetMapping("/profil")
+    public ModelAndView startZeiteintrag(){
+        ModelAndView modelView = new ModelAndView();
+        modelView.setViewName("profilUebersicht");
+        return modelView;
+    }
+}
