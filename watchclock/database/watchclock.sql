89d6955cc3902d9a93d0f9f4a8c40b289f86de62
annika <annika.pass@gmx.de>
2021-05-17 18:59:46 +0200

    geänderte db

--- watchclock/database/watchclock.sql
+++ watchclock/database/watchclock.sql
@@ -2,7 +2,7 @@ drop table zeiteintrag;
 drop table users;
 
 Create table users (
-	personal_Nr INT(6) auto_increment primary key,
+	personalNr INT(6) auto_increment primary key,
     vorname VARCHAR(30) NOT NULL,
 	nachname VARCHAR(30) NOT NULL,
 	email VARCHAR(50) NOT NULL,
@@ -10,21 +10,22 @@ Create table users (
 	geburtsdatum date,
     soll_Arbeitszeit INT (2),
     passwort binary (100) NOT NULL,
-    ist_Admin boolean);
+    ist_Admin boolean,
+    is_enabled boolean);
 
-Insert into users values (1, "Annika", "Paß", "annika.pass@gmx.de", "annika.pass", "1998-01-21", 40,"$2y$12$zdF/W2rjaTusmEeHDQEsb.hOtCtYntdrC/D7foM72QoaXZTOj4Yuu", true);
-Insert into users values (2, "Juliane", "Ruß", "juliane.russ@gmx.de", "juliane.russ", "1998-01-21", 40,"$2y$12$zdF/W2rjaTusmEeHDQEsb.hOtCtYntdrC/D7foM72QoaXZTOj4Yuu", false); 
+Insert into users values (1, "Annika", "Paß", "annika.pass@gmx.de", "annika.pass", "1998-01-21", 40,"abcd", true, true);
+Insert into users values (2, "Juliane", "Ruß", "juliane.russ@gmx.de", "juliane.russ", "1998-01-21", 40,"$2y$12$zdF/W2rjaTusmEeHDQEsb.hOtCtYntdrC/D7foM72QoaXZTOj4Yuu", false, true);
 
 
 create table zeiteintrag(
-	eintrag_Nr Int(6) auto_increment primary key,
+	eintragNr Int(6) auto_increment primary key,
     datum date not null,
     von datetime,
     bis datetime,
     kommentar varchar(100),
-    personal_Nr int(6),
-    foreign key (personal_Nr) references users(personal_Nr));
-    
+    personalNr int(6),
+    foreign key (personalNr) references users(personalNr));
+
 insert into zeiteintrag values(1, "20.04.21", "9.00.00", "15.00.00","", 1);
 
 Select * from users;
