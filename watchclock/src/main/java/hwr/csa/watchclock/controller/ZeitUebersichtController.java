9ffe2ae6cf8eaa66ca1f8debe0657e726d383f0d
annika <annika.pass@gmx.de>
2021-05-21 19:39:29 +0200

    ZeitAendern.html + anzeige Controller

--- watchclock/src/main/java/hwr/csa/watchclock/controller/ZeitUebersichtController.java
+++ watchclock/src/main/java/hwr/csa/watchclock/controller/ZeitUebersichtController.java
@@ -1,11 +1,23 @@
 package hwr.csa.watchclock.controller;
 
+import hwr.csa.watchclock.modell.Zeiteintrag;
+import hwr.csa.watchclock.modell.ZeiteintragRepository;
+import hwr.csa.watchclock.view.ZeitAendernView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.Time;
+import java.sql.Date;
 
 @Controller
 public class ZeitUebersichtController {
+
+    @Autowired
+    ZeiteintragRepository zeiteintragRepository;
 
     @GetMapping("/zeitUebersicht")
     public ModelAndView startTest2(){
@@ -13,4 +25,39 @@ public class ZeitUebersichtController {
         modelView.setViewName("zeitUebersicht");
         return modelView;
     }
+
+    @GetMapping("/zeitUebersicht/zeitAendern/{zeiteintragNr}")
+    public ModelAndView GetZeitAendern(@PathVariable("zeiteintragNr") int zeiteintragNr){
+        ModelAndView modelAndView = new ModelAndView();
+        ZeitAendernView zeitAendernView = new ZeitAendernView();
+        Zeiteintrag zeiteintrag = zeiteintragRepository.findByEintragNr(zeiteintragNr);
+
+        zeitAendernView.setZeitEintragNr(zeiteintragNr);
+        zeitAendernView.setTag(new Date(zeiteintrag.getVon().getTime()));
+        zeitAendernView.setVon(new Time(zeiteintrag.getVon().getTime()));
+        zeitAendernView.setBis(new Time(zeiteintrag.getBis().getTime()));
+        zeitAendernView.setKommentar(zeiteintrag.getKommentar());
+
+        modelAndView.addObject("view", zeitAendernView);
+        modelAndView.setViewName("zeitAendern");
+        return modelAndView;
+    }
+
+    @PostMapping("/zeitUebersicht/zeitAendern/{zeiteintragNr}")
+    public ModelAndView GetZeitAendern(@PathVariable("zeiteintragNr") int zeiteintragNr, ModelAndView modelAndView){
+
+        ZeitAendernView zeitAendernView = new ZeitAendernView();
+        Zeiteintrag zeiteintrag = zeiteintragRepository.findByEintragNr(zeiteintragNr);
+
+        zeitAendernView.setZeitEintragNr(zeiteintragNr);
+        zeitAendernView.setTag(new Date(zeiteintrag.getVon().getTime()));
+        zeitAendernView.setVon(new Time(zeiteintrag.getVon().getTime()));
+        zeitAendernView.setBis(new Time(zeiteintrag.getVon().getTime()));
+
+        modelAndView.addObject("view", zeitAendernView);
+        modelAndView.setViewName("zeitAendern");
+        return modelAndView;
+    }
+
+
 }
---
+++ watchclock/src/main/java/hwr/csa/watchclock/view/ZeitAendernView.java
@@ -0,0 +1,70 @@
+package hwr.csa.watchclock.view;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class ZeitAendernView {
+    private long zeitEintragNr;
+    private Date tag;
+    private Time von;
+    private Time bis;
+    private String kommentar;
+    private boolean error;
+    private boolean errormsg;
+
+    public long getZeitEintragNr() {
+        return zeitEintragNr;
+    }
+
+    public void setZeitEintragNr(long zeitEintragNr) {
+        this.zeitEintragNr = zeitEintragNr;
+    }
+
+    public Date getTag() {
+        return tag;
+    }
+
+    public void setTag(Date tag) {
+        this.tag = tag;
+    }
+
+    public Time getVon() {
+        return von;
+    }
+
+    public void setVon(Time von) {
+        this.von = von;
+    }
+
+    public Time getBis() {
+        return bis;
+    }
+
+    public void setBis(Time bis) {
+        this.bis = bis;
+    }
+
+    public String getKommentar() {
+        return kommentar;
+    }
+
+    public void setKommentar(String kommentar) {
+        this.kommentar = kommentar;
+    }
+
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
+
+    public boolean isErrormsg() {
+        return errormsg;
+    }
+
+    public void setErrormsg(boolean errormsg) {
+        this.errormsg = errormsg;
+    }
+}
